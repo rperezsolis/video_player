2a5978d66bf29d4d8d62e3b0417808d0b9ef41c5
rperezsolis <irafael.perez826@gmail.com>
2020-06-23 21:01:46 -0500

    Camera Preview ready

--- app/src/main/java/com/rafaelperez/videoplayer/ui/VideoFragment.kt
+++ app/src/main/java/com/rafaelperez/videoplayer/ui/VideoFragment.kt
@@ -1,18 +1,77 @@
 package com.rafaelperez.videoplayer.ui
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.snackbar.Snackbar
+import com.google.common.util.concurrent.ListenableFuture
 import com.rafaelperez.videoplayer.R
 import com.rafaelperez.videoplayer.databinding.FragmentVideoBinding
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class VideoFragment : Fragment() {
+    private lateinit var binding: FragmentVideoBinding
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding: FragmentVideoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_video, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_video, container, false)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
         return binding.root
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            Runnable {
+                val cameraProvider = cameraProviderFuture.get()
+
+                val preview : Preview = Preview.Builder().build()
+
+                val cameraSelector : CameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                    .build()
+
+                cameraProvider.unbindAll()
+                val camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+                preview.setSurfaceProvider(binding.previewView.createSurfaceProvider(camera.cameraInfo))
+            }, ContextCompat.getMainExecutor(requireContext())
+        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireActivity().baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Snackbar.make(requireView(), "Permissions not granted by the user.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
